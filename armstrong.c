45623922a567bf5ede99980aac4c3797295a6e15
irfanzakhir <irfanzakheer@gmail.com>
2025-04-03 11:28:20 +0530

    1st commit

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int  n,s=0 ,temp ,x;
+printf("enter the number");
+scanf("%d",&n);
+temp=n;
+while(n>0)
+{
+x=n%10;
+s=s+(x*x*x);
+n=n/10;
+}
+if(temp==s)
+printf("%d is an armstrong number",temp);
+else
+printf("%d is not an armstrong number",temp);
+}
